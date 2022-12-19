1c5f61940ab10a317eb7f6b68f13484e594b0d22
ChKhasan <chulliyevhasan1405@mail.ru>
2022-12-19 18:27:25 +0500

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "wood-site",
+    title: "Woodline",
     target: "server",
     htmlAttrs: {
       lang: "en",
